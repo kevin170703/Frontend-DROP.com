a593e2f75b789e1c99bb3801a17f25394aa2a2d7
kevin170703 <correakevinfabian01@gmail.com>
2023-02-03 14:16:15 -0300

    componente navegacion reecho

--- src/components/Navegation/Navegation.jsx
+++ src/components/Navegation/Navegation.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import style from "./Navegation.module.css";
-import { CiSearch, CiCirclePlus } from "react-icons/ci";
+import { CiSearch, CiCirclePlus, CiCircleList } from "react-icons/ci";
 
 export default function Navegation() {
   return (
     <div className={style.contentAll}>
-      <h1>Productos</h1>
       <form action="" className={style.search}>
         <CiSearch size="40" color="fff" />
         <input type="text" placeholder="Buscar" />
       </form>
 
+      <div className={style.menus}>
+        <CiCircleList size="35" />
+        <p>Lista de productos</p>
+      </div>
       <div className={style.menus}>
         <CiCirclePlus size="35" />
         <p>Nuevo</p>
