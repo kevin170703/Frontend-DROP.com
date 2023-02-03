e4436f9985aa67d75ee95a3524dbf73ebb99ad85
kevin170703 <correakevinfabian01@gmail.com>
2023-02-03 14:15:44 -0300

    componente home añadido

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,53 @@
 import React from "react";
 import style from "./Home.module.css";
-import Navegation from "../Navegation/Navegation";
+import DataIamge from "../../images/DataImage.svg";
+import dashboard from "../../images/dashboard.png";
+import { NavLink } from "react-router-dom";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FiMail } from "react-icons/fi";
+import { IoIosArrowDown } from "react-icons/io";
 
 export default function Home() {
   return (
-    <div className={style.content}>
-      <Navegation />
-      <div className={style.contentList}>
-        <h6>Mis productos</h6>
+    <div className={style.contentAll}>
+      <div className={style.contentPresentation}>
+        <div className={style.contentText}>
+          <h1>Titulo</h1>
+          <p>
+            Guarda tus datos, productos, los que desees, de forma facil y
+            sensillla.
+          </p>
+          <NavLink to="/listProducts" className={style.buutonLestsGo}>
+            Empezar
+          </NavLink>
+        </div>
+        <img src={DataIamge} alt="" />
+        <div className={style.networks}>
+          <a
+            href="https://www.linkedin.com/in/kevin-correa-dev/"
+            target="_blank"
+          >
+            <FaLinkedinIn size="30" />
+          </a>
+          <a
+            href="mailto:correakevinfabian01@gmail.com"
+            target="_BLANK"
+            rel="noopener noreferrer"
+          >
+            <FiMail size="30" />
+          </a>
+        </div>
+        <IoIosArrowDown size="30" className={style.arrow} />
+      </div>
+      <div className={style.info}>
+        <div className={style.contentInfo}>
+          <h6>Dashborad facil de usar</h6>
+          <p>
+            Puedes buscar, crear, guardar, editar, eliminar tus datos de manera
+            rapida en un dashboard comodo de utilizar e intutivo.
+          </p>
+        </div>
+        <img src={dashboard} alt="" />
       </div>
     </div>
   );
