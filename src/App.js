44909a6b1015abf40d6e0a395d949d66efe136d7
kevin170703 <correakevinfabian01@gmail.com>
2023-02-04 14:05:50 -0300

    añadiendo rutas

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Home from "./components/Home/Home";
 import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/listProducts" element={<Dashboard />} />
         <Route path="/" element={<Home />} />
+        <Route path="/listProducts" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
