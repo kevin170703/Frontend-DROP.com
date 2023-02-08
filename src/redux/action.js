a771fe66d1b1f00d777a34e117d812bc5a2bcfe1
kevin170703 <correakevinfabian01@gmail.com>
2023-02-07 22:17:11 -0300

    action para registrarse

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,13 +1,9 @@
-// export function getServices() {
-//   return async function (distpach) {
-//     const services = await axios("/posts");
-//     return distpach({ type: "GET_SERVICES", payload: services.data });
-//   };
-// }
+import axios from "axios";
 
 export const register = (data) => {
+  console.log(data);
   return async function (distpach) {
-    const user = await axios.put("http://localhost:3001/users/create", data);
+    const user = await axios.post("http://localhost:3001/users/create", data);
     return distpach({ type: "REGISTER", payload: user.data });
   };
 };
