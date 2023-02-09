021934438543f28f8fb9bc9feb37eddcb9f8f5de
kevin170703 <correakevinfabian01@gmail.com>
2023-02-09 11:05:24 -0300

    localstorage agregado

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,12 +1,21 @@
 const initialState = {
-  user: {},
+  user: localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user"))
+    : {},
   posts: [],
 };
+
+function setInLocalStorage(key, state) {
+  localStorage.setItem(key, JSON.stringify(state));
+  return state;
+}
 
 export default function reducer(state = initialState, { type, payload }) {
   switch (type) {
     case "REGISTER":
-      return { user: payload };
+      return { ...state, user: setInLocalStorage("user", payload) };
+    case "LOGIN":
+      return { ...state, user: setInLocalStorage("user", payload) };
     default:
       return { ...state };
   }
