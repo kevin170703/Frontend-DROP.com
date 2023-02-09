2380b034286e630061c8e2a99cecf55cedf990b3
kevin170703 <correakevinfabian01@gmail.com>
2023-02-09 11:04:03 -0300

    añadiendo logo para perfil y su logica

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import style from "./NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { FaUserAlt } from "react-icons/fa";
 
 export default function NavBar() {
+  const user = useSelector((state) => state.user);
+
   return (
     <div className={style.contentAll}>
-      <NavLink to="/login" className={style.buutons}>
-        Iniciar sesion
-      </NavLink>
-      <NavLink to="/register" className={style.buutons}>
-        Registrate
-      </NavLink>
+      {user.email ? (
+        <NavLink to="/profile">
+          <FaUserAlt size="30" className={style.profileLogo} />
+        </NavLink>
+      ) : (
+        <div>
+          <NavLink to="/login" className={style.buutons}>
+            Iniciar sesion
+          </NavLink>
+          <NavLink to="/register" className={style.buutons}>
+            Registrate
+          </NavLink>
+        </div>
+      )}
     </div>
   );
 }
