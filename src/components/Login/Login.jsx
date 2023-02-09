aa5f2489c374ee9ec49661be5ef861ff6e589423
kevin170703 <correakevinfabian01@gmail.com>
2023-02-09 11:03:26 -0300

    logica de iniciar sesion denegada

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Login.module.css";
 import { NavLink } from "react-router-dom";
-import { FaLinkedinIn } from "react-icons/fa";
-import { FiMail } from "react-icons/fi";
+import { login } from "../../redux/action";
+import { useDispatch } from "react-redux";
+import Swal from "sweetalert2";
 
 export default function Login() {
+  const dispatch = useDispatch();
+  const [dataUser, setDataUser] = useState({
+    userName: "",
+    password: "",
+  });
+  const [errors, seterrors] = useState({});
+
+  function updateState(e) {
+    e.preventDefault();
+    setDataUser({ ...dataUser, [e.target.name]: e.target.value });
+    seterrors(validations({ ...dataUser, [e.target.name]: e.target.value }));
+  }
+
+  function validations(dataUser) {
+    let errors = {};
+    if (!dataUser.userName)
+      errors.userName = "Debe ingresar el nombre de usuario";
+
+    if (!dataUser.password) errors.password = "Debe ingresar la contraseña";
+    return errors;
+  }
+
+  function setData(e) {
+    e.preventDefault();
+    if (dataUser.userName === "" || dataUser.password === "")
+      return Swal.fire({
+        icon: "error",
+        title: "Algun campo se encuentra incompeto",
+      });
+    dispatch(login(dataUser));
+  }
+
   return (
     <div className={style.contentAll}>
       <div className={style.contentImage}>
@@ -14,18 +47,32 @@ export default function Login() {
           Registrarme
         </NavLink>
       </div>
-      <form action="" className={style.formLogin}>
+      <form action="" className={style.formLogin} onSubmit={(e) => setData(e)}>
         <h6>Iniciar sesion</h6>
         <div className={style.contentInputs}>
-          <input type="text" placeholder="Correo electronico" />
+          <input
+            type="text"
+            placeholder="Nombre de usuario"
+            name="userName"
+            value={dataUser.userName}
+            onChange={(e) => updateState(e)}
+          />
+          {errors.userName && <p className={style.errors}>{errors.userName}</p>}
         </div>
         <div className={style.contentInputs}>
-          <input type="text" placeholder="Contraseña" />
+          <input
+            type="text"
+            placeholder="Contraseña"
+            name="password"
+            value={dataUser.password}
+            onChange={(e) => updateState(e)}
+          />
+          {errors.password && <p className={style.errors}>{errors.password}</p>}
         </div>
 
-        <NavLink to="login" className={style.buutonLogin}>
+        <button type="submit" to="login" className={style.buutonLogin}>
           Iniciar sesion
-        </NavLink>
+        </button>
       </form>
     </div>
   );
