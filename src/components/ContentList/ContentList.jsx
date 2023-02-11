df9bfeabb6beef3a7b5c156b1ef5558d79e91c4c
kevin170703 <correakevinfabian01@gmail.com>
2023-02-10 21:02:56 -0300

    agregando funcionalidad a contentList

--- src/components/ContentList/ContentList.jsx
+++ src/components/ContentList/ContentList.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import style from "./ContentList.module.css";
 import Products from "../Products/Products";
+import { useDispatch, useSelector } from "react-redux";
+import { searchProductsforUser } from "../../redux/action";
 
 export default function ContentList() {
+  const user = useSelector((state) => state.user);
+  const posts = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(searchProductsforUser(user.id));
+  }, []);
+
+  if (!user.email) return <h1>ERROR</h1>;
   return (
     <div className={style.contentList}>
       <h6>Datos guardados</h6>
@@ -17,8 +28,21 @@ export default function ContentList() {
           <p>Cantidad</p>
         </div>
       </div>
-      <Products TitleProduct="Dato 1" price="240" quantity="10" />
-      <Products TitleProduct="Dato 2" price="200" quantity="20" />
+      {posts.msj ? (
+        <div className={style.contentMsj}>
+          <h6>Aun no cuentas con datos guardados</h6>
+        </div>
+      ) : (
+        posts.map((element) => (
+          <Products
+            key={element.id}
+            TitleProduct={element.nameProduct}
+            price={element.price}
+            quantity={element.stock}
+            id={element.id}
+          />
+        ))
+      )}
     </div>
   );
 }
