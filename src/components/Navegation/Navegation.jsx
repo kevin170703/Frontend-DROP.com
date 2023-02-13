a56ceba1682f462e8c1da7cb51018f4df7d020cb
kevin170703 <correakevinfabian01@gmail.com>
2023-02-13 15:03:14 -0300

    añadiendo funcionalidad a botones

--- src/components/Navegation/Navegation.jsx
+++ src/components/Navegation/Navegation.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 import style from "./Navegation.module.css";
 import { CiSearch, CiCirclePlus, CiCircleList } from "react-icons/ci";
+import { NavLink } from "react-router-dom";
+import { BiLeftArrowAlt } from "react-icons/bi";
 
 export default function Navegation() {
   return (
     <div className={style.contentAll}>
+      <NavLink to="/" className={style.buttonBack}>
+        <BiLeftArrowAlt size="40" />
+      </NavLink>
       <form action="" className={style.search}>
         <CiSearch size="40" color="fff" />
         <input type="text" placeholder="Buscar" />
       </form>
 
-      <div className={style.menus}>
+      {/* <div className={style.menus}>
         <CiCircleList size="35" />
         <p>Lista de productos</p>
-      </div>
-      <div className={style.menus}>
+      </div> */}
+      <NavLink to="/createProduct" className={style.menus}>
         <CiCirclePlus size="35" />
         <p>Nuevo</p>
-      </div>
+      </NavLink>
     </div>
   );
 }
