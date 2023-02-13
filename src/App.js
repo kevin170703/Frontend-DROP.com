8bfede647c15bd583893af5a36880d5d9eb277a2
kevin170703 <correakevinfabian01@gmail.com>
2023-02-13 15:05:16 -0300

    agregando rutas y modificando otras

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Home from "./components/Home/Home";
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import CreateProduct from "./components/CreateProduct/CreateProduct";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/listProducts" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/createProduct" element={<CreateProduct />} />
       </Routes>
     </div>
   );
