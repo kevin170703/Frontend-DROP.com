9c1232907444424ab906d6545e72980b26acb608
kevin170703 <correakevinfabian01@gmail.com>
2023-03-14 14:04:28 -0300

    anñadiendo annimaciones

--- src/components/Navegation/Navegation.jsx
+++ src/components/Navegation/Navegation.jsx
@@ -3,10 +3,16 @@ import style from "./Navegation.module.css";
 import { CiSearch, CiCirclePlus, CiCircleList } from "react-icons/ci";
 import { NavLink } from "react-router-dom";
 import { BiLeftArrowAlt } from "react-icons/bi";
+import { motion } from "framer-motion";
 
 export default function Navegation() {
   return (
-    <div className={style.contentAll}>
+    <motion.div
+      className={style.contentAll}
+      initial={{ opacity: 0, x: -20 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
       <NavLink to="/" className={style.buttonBack}>
         <BiLeftArrowAlt size="40" />
       </NavLink>
@@ -23,6 +29,6 @@ export default function Navegation() {
         <CiCirclePlus size="35" />
         <p>Nuevo</p>
       </NavLink>
-    </div>
+    </motion.div>
   );
 }
