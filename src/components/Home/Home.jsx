34e1e992706b487a25ad6b4b409c9ac640e56d7c
kevin170703 <correakevinfabian01@gmail.com>
2023-03-14 14:04:00 -0300

    Desiseño completo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import style from "./Home.module.css";
-import DataIamge from "../../images/DataImage.svg";
+import { motion, useScroll } from "framer-motion";
 import dashboard from "../../images/dashboard.png";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FiMail } from "react-icons/fi";
 import { IoIosArrowDown } from "react-icons/io";
 import Swal from "sweetalert2";
 import NavBar from "../NavBar/NavBar";
-import { NavLink } from "react-router-dom";
+
 import { useSelector } from "react-redux";
 
 export default function Home() {
@@ -18,19 +18,35 @@ export default function Home() {
       <NavBar />
       <div className={style.contentPresentation}>
         <div className={style.contentText}>
-          <h1>Titulo</h1>
-          <p>
+          <motion.h1
+            initial={{ y: -100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.3, duration: 0.5 }}
+          >
+            Titulo
+          </motion.h1>
+          <motion.p
+            initial={{ x: -100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.5, duration: 0.8 }}
+          >
             Guarda tus datos, productos, los que desees, de forma facil y
             sensillla.
-          </p>
+          </motion.p>
           {user.email ? (
-            <NavLink to="/dashboard" className={style.buutonLestsGo}>
+            <motion.button
+              onClick={() => (window.location.href = "/dashboard")}
+              className={style.buutonLestsGo}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, scale: [0.5, 1.1, 1] }}
+              transition={{ delay: 1, duration: 0.8 }}
+            >
               Empezar
-            </NavLink>
+            </motion.button>
           ) : (
             <button
               className={style.buutonLestsGo}
-              onClick={() =>
+              onClick={() => {
                 Swal.fire({
                   icon: "error",
                   title: `debes iniciar sesion o crear un cuenta antes de empezar`,
@@ -43,14 +59,14 @@ export default function Home() {
                   } else if (result.isDismissed) {
                     window.location.href = "/register";
                   }
-                })
-              }
+                });
+              }}
             >
               Empezar
             </button>
           )}
         </div>
-        <img src={DataIamge} alt="" />
+        {/* <img src={Data} alt="" /> */}
         <div className={style.networks}>
           <a
             href="https://www.linkedin.com/in/kevin-correa-dev/"
@@ -69,14 +85,28 @@ export default function Home() {
         <IoIosArrowDown size="30" className={style.arrow} />
       </div>
       <div className={style.info}>
-        <div className={style.contentInfo}>
+        <motion.div
+          className={style.contentInfo}
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true, amount: 0.6 }}
+          transition={{ duration: 1 }}
+        >
           <h6>Dashborad facil de usar</h6>
           <p>
             Puedes buscar, crear, guardar, editar, eliminar tus datos de manera
             rapida en un dashboard comodo de utilizar e intutivo.
           </p>
-        </div>
-        <img src={dashboard} alt="" />
+        </motion.div>
+        <motion.img
+          src={dashboard}
+          alt="Dashboard image"
+          className={style.contentInfo}
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          viewport={{ once: true, amount: 0.6 }}
+          transition={{ duration: 1, delay: 0.2 }}
+        />
       </div>
     </div>
   );
