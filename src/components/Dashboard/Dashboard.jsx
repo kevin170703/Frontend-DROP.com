20293b9f50eedd3d93e30e6a3f0934a2b63b2598
kevin170703 <correakevinfabian01@gmail.com>
2023-04-04 10:52:42 -0300

    mudando proyecto a uploader de fotos

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import style from "./Dashboard.module.css";
 import Navegation from "../Navegation/Navegation";
-import ContentList from "../ContentList/ContentList";
+import Upload from "../Upload/Upload";
 
 export default function Home() {
   return (
     <div className={style.content}>
       <Navegation />
-      <ContentList />
+      <Upload />
     </div>
   );
 }
