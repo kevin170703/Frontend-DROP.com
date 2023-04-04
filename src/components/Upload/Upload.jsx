bcccb0a646fd7b08710066f497936dfa5ed0227d
kevin170703 <correakevinfabian01@gmail.com>
2023-04-04 15:59:33 -0300

    proyecto de upload imagenes

--- src/components/Upload/Upload.jsx
+++ src/components/Upload/Upload.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import style from "./Upload.module.css";
 import uploadCard from "../../images/uploadCard.png";
+import Navegation from "../Navegation/Navegation";
 
 export default function Upload() {
   return (
     <div className={style.contentAll}>
-      <h1>Sube tu imagen</h1>
-      <p>Tus imagenes favoritas</p>
+      <Navegation uploadPhotos={true} />
+      <div className={style.contentUploader}>
+        <h1>Sube tu imagen</h1>
+        <p>Tus imagenes favoritas</p>
 
-      <label for="hola">
-        <img src={uploadCard} alt="" />
-        <input id="hola" type="file" accept="image/png,image/jpeg" />
-      </label>
+        <label for="hola">
+          <img src={uploadCard} alt="" />
+          <input id="hola" type="file" accept="image/png,image/jpeg" />
+        </label>
+      </div>
     </div>
   );
 }
