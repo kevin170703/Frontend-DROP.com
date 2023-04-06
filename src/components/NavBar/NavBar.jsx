61210d5db860f34f4959dbb991e21c81c85ec827
kevin170703 <correakevinfabian01@gmail.com>
2023-04-06 16:39:08 -0300

    logica par iniciar sesion

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import style from "./NavBar.module.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { FaUserAlt } from "react-icons/fa";
 import { motion } from "framer-motion";
+import Swal from "sweetalert2";
 
 export default function NavBar() {
   const user = useSelector((state) => state.user);
+  const navegation = useNavigate();
+  const alert = () => {
+    Swal.fire({
+      icon: "info",
+      title: "debes iniciar sesion para poder comenzar",
+      confirmButtonText: "Iniciar sesion",
+      confirmButtonColor: "#2c8cf8",
+      showCancelButton: true,
+      cancelButtonText: "Hacerlo mas tarde",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        navegation("/login");
+      }
+    });
+  };
 
   return (
     <div className={style.contentAll}>
@@ -14,13 +30,23 @@ export default function NavBar() {
         <NavLink to="/" className={style.title}>
           DROP.com
         </NavLink>
-        <NavLink to="/uploadPhoto" className={style.navegationLinks}>
-          Empezar
-        </NavLink>
+        {user.id ? (
+          <NavLink to="/uploadPhoto" className={style.navegationLinks}>
+            Empezar
+          </NavLink>
+        ) : (
+          <NavLink
+            to="/"
+            className={style.navegationLinks}
+            onClick={() => alert()}
+          >
+            Empezar
+          </NavLink>
+        )}
       </div>
 
       {user.email ? (
-        <NavLink to="/profile/:id">
+        <NavLink to={`/profile/${user.id}`}>
           <FaUserAlt size="30" className={style.profileLogo} />
         </NavLink>
       ) : (
