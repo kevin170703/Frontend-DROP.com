b3727b38dc53cacc66ae33dab9fa3e07d9111c76
kevin170703 <correakevinfabian01@gmail.com>
2023-04-06 16:40:56 -0300

    logica par ver y descargar fotos

--- src/components/Myphoto/MyPhoto.jsx
+++ src/components/Myphoto/MyPhoto.jsx
@@ -4,26 +4,71 @@ import style from "./MyPhoto.module.css";
 import { getImages } from "../../redux/action.js";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { IoIosClose } from "react-icons/io";
+import fileDownload from "js-file-download";
+import axios from "axios";
 
 export default function MyPhoto() {
-  const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getImages(user.id));
   }, []);
 
+  const user = useSelector((state) => state.user);
   const images = useSelector((state) => state.images);
+
+  const [imageView, setImageView] = useState({
+    view: false,
+    image: "",
+  });
+
+  const dowloadFile = async (imageUrl, nameImage) => {
+    const response = await axios.get(imageUrl, { responseType: "blob" });
+    const blob = response.data;
+    fileDownload(blob, `${nameImage}.${blob.type.slice(6)}`);
+  };
+
   return (
     <div className={style.contentAll}>
-      <Navegation myPhotos={true} />
+      {imageView.view && (
+        <div className={style.imageView}>
+          <button onClick={() => setImageView({ view: false, image: "" })}>
+            <IoIosClose size="50" />
+          </button>
+          <img src={imageView.image} alt="" />
+        </div>
+      )}
+      <Navegation myPhotos={true} className={style.navegation} />
       <div className={style.contentPhotos}>
-        <h1>Mis Fotos</h1>
-        {images.map((photo) =>
+        <div className={style.contentText}>
+          <h1>Mis Fotos</h1>
+          <p>Aca puedes ver todas tus fotos</p>
+        </div>
+        {images.map((photo, index) =>
           photo.msj ? (
             <h4>No cuentas con imagenes aun</h4>
           ) : (
-            <img src={photo.imageUrl} alt="" />
+            <div className={style.contentImage} key={index}>
+              <img src={photo.imageUrl} alt="" />
+              <div className={style.buttonsActions}>
+                <button
+                  className={style.buttonsAction}
+                  onClick={() => dowloadFile(photo.imageUrl, photo.name)}
+                >
+                  Desacargar
+                </button>
+                <button
+                  className={style.buttonsAction}
+                  onClick={() =>
+                    setImageView({ view: true, image: photo.imageUrl })
+                  }
+                >
+                  Ver
+                </button>
+              </div>
+            </div>
           )
         )}
       </div>
