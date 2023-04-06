f2c845d03cf3e96816b297c06cc9a180660e83ac
kevin170703 <correakevinfabian01@gmail.com>
2023-04-06 20:14:50 -0300

    preparando y añadiendo logica para ver el almacenamiento total

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Navegation from "../Navegation/Navegation";
 import style from "./Profile.module.css";
 
 export default function Profile() {
-  return <div className={style.contentAll}>Profile</div>;
+  const images = useSelector((state) => state.images);
+  const totalSizeImages = images.reduce(
+    (acumulador, image) => image.size + acumulador,
+    0
+  );
+  const totalSize = (totalSizeImages.toString().slice(0, -3) / 1024).toFixed(1);
+
+  return (
+    <div className={style.contentAll}>
+      <Navegation profile={true} />
+      <p>Total de almacenamiento: {totalSize} MB</p>
+    </div>
+  );
 }
