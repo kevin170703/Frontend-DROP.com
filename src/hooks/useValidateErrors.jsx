9a9f50d87ef6431d0fa4cc1ab36861127efad03f
kevin170703 <correakevinfabian01@gmail.com>
2023-05-08 15:16:54 -0300

    Añadiendo validacion para login

--- src/hooks/useValidateErrors.jsx
+++ src/hooks/useValidateErrors.jsx
@@ -7,46 +7,44 @@ export function useValidateErrors() {
   const isFirstInputEmail = useRef(true);
   const isFirstInputPassword = useRef(true);
 
-  function validateErrors(dataUser) {
+  function validateErrorsRegister(dataUser) {
     let errors = {};
+    const { userName, email, password } = dataUser;
+
     const emailRegex =
       /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     const userNameRegex = /^[0-9a-zA-Z]+$/;
     const passwordRegex = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/;
 
-    if (
-      dataUser.userName === "" ||
-      dataUser.email === "" ||
-      dataUser.password === ""
-    )
+    if (userName === "" || email === "" || password === "")
       return setErrors({ exist: true });
 
     if (isFirstInputUserName.current) {
-      isFirstInputUserName.current = dataUser.userName === "";
-    } else if (!dataUser.userName) {
+      isFirstInputUserName.current = userName === "";
+    } else if (!userName) {
       errors.exist = true;
       errors.userName = "Debe ingresar un nombre de usuario";
-    } else if (!userNameRegex.test(dataUser.userName)) {
+    } else if (!userNameRegex.test(userName)) {
       errors.exist = true;
       errors.userName = "Solo pude contener numeros y letras";
     }
 
     if (isFirstInputEmail.current) {
-      isFirstInputEmail.current = dataUser.email === "";
-    } else if (!dataUser.email) {
+      isFirstInputEmail.current = email === "";
+    } else if (!email) {
       errors.exist = true;
       errors.email = "Debe ingresar su correo";
-    } else if (!emailRegex.test(dataUser.email)) {
+    } else if (!emailRegex.test(email)) {
       errors.exist = true;
       errors.email = "Email invalido";
     }
 
     if (isFirstInputPassword.current) {
-      isFirstInputPassword.current = dataUser.password === "";
-    } else if (!dataUser.password) {
+      isFirstInputPassword.current = password === "";
+    } else if (!password) {
       errors.exist = true;
       errors.password = "Debe ingresar una contraseña";
-    } else if (!passwordRegex.test(dataUser.password)) {
+    } else if (!passwordRegex.test(password)) {
       errors.exist = true;
       errors.password = (
         <p>
@@ -59,6 +57,18 @@ export function useValidateErrors() {
     }
     setErrors(errors);
   }
-  function validateErrorsSubmit(dataUser) {}
-  return { validateErrors, validateErrorsSubmit, errors };
+
+  function validationsLogin(dataUser) {
+    let errors = {};
+    const { userName, password } = dataUser;
+
+    if (userName === "" || password === "") {
+      errors.exist = true;
+      return setErrors(errors);
+    }
+
+    setErrors(errors);
+  }
+
+  return { validateErrorsRegister, validationsLogin, errors };
 }
